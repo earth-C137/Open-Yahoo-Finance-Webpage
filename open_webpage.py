389576f7731804664c3b0849c17fd0e88158199e
earth-C137 <42741144+earth-C137@users.noreply.github.com>
2020-04-11 11:25:33 -0500

    Update open_webpage.py

--- open_webpage.py
+++ open_webpage.py
@@ -1,11 +1,25 @@
+"""
+A simple module with three functions to open different yahoo finance web pages.
+The argument is a stock ticker symbol passed in as a string.
+"""
+
 import webbrowser
 
+def yahoo_finance_summary(ticker):
+    webpage = "https://finance.yahoo.com/quote/{}?p={}&.tsrc=fin-srch".format(ticker, ticker)
+    webbrowser.open(webpage, autoraise=True)
 
 def yahoo_finance_profile(ticker):
-        webpage = "https://finance.yahoo.com/quote/{}/profile?p={}".format(ticker, ticker)
-        webbrowser.open(webpage, autoraise=True)
+    webpage = "https://finance.yahoo.com/quote/{}/profile?p={}".format(ticker, ticker)
+    webbrowser.open(webpage, autoraise=True)
+
+def yahoo_finance_options(ticker):
+    webpage = "https://finance.yahoo.com/quote/{}/options?p={}".format(ticker, ticker)
+    webbrowser.open(webpage, autoraise=True)
 
 if __name__ == '__main__':
+    #for example and to test
     list = ['AMZN', 'BA']
     for each in list:
-        yahoo_finance_profile(each)
+        yahoo_finance_summary(each)
+
