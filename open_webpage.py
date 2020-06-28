91ba5ef9b526daa053bcb6cfd145b94ff089d3cd
earth-C137 <42741144+earth-C137@users.noreply.github.com>
2020-06-28 10:22:50 -0500

    Edited format to confirm to PEP 8 line length.

--- open_webpage.py
+++ open_webpage.py
@@ -6,15 +6,24 @@ The argument is a stock ticker symbol passed in as a string.
 import webbrowser
 
 def yahoo_finance_summary(ticker):
-    webpage = "https://finance.yahoo.com/quote/{}?p={}&.tsrc=fin-srch".format(ticker, ticker)
+    webpage = (
+        "https://finance.yahoo.com/quote/{}?p={}&.tsrc=fin-srch"
+        .format(ticker, ticker)
+        )
     webbrowser.open(webpage, autoraise=True)
 
 def yahoo_finance_profile(ticker):
-    webpage = "https://finance.yahoo.com/quote/{}/profile?p={}".format(ticker, ticker)
+    webpage = (
+        "https://finance.yahoo.com/quote/{}/profile?p={}"
+        .format(ticker, ticker)
+        )
     webbrowser.open(webpage, autoraise=True)
 
 def yahoo_finance_options(ticker):
-    webpage = "https://finance.yahoo.com/quote/{}/options?p={}".format(ticker, ticker)
+    webpage = (
+        "https://finance.yahoo.com/quote/{}/options?p={}"
+        .format(ticker, ticker)
+        )
     webbrowser.open(webpage, autoraise=True)
 
 if __name__ == '__main__':
